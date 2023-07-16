351bdfa226808f4cb43130def3c3c99c707ef145
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-16 12:10:15 +0200

    Changes + status bar main window

---
+++ CaloriesPFC_Calculator_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CaloriesPFC_Calculator_WPF.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriesPFC_Calculator_WPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Title property : string
+        private string _title = "CPFC_Calculator";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+    }
+}
