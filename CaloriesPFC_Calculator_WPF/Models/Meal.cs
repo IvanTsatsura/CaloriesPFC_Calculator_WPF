fa34201b4e2d4a04b6903c5fb61bf62ef8461377
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-17 00:05:38 +0200

    Class Product + change class Meal

--- CaloriesPFC_Calculator_WPF/Models/Meal.cs
+++ CaloriesPFC_Calculator_WPF/Models/Meal.cs
@@ -9,11 +9,94 @@ namespace CaloriesPFC_Calculator_WPF.Models
 {
     internal class Meal
     {
-        public string Name { get; set; }
-        public float Calories { get; set; }
-        public float Proteins { get; set; }
-        public float Fats { get; set; }
-        public float Carbohydrates { get; set; }
+        public IEnumerable<Product>? Products { get; set; }
         public MealType Type { get; set; }
+        private float _calories = 0;
+        public float Calories
+        {
+            get
+            {
+                if (Products != null)
+                {
+                    foreach (Product product in Products)
+                    {
+                        _calories += product.Calories;
+                    }
+                }
+
+                return _calories;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException(nameof(value));
+                _calories = value;
+            }
+        }
+        private float _proteins;
+        public float Proteins
+        {
+            get
+            {
+                if (Products != null)
+                {
+                    foreach (Product product in Products)
+                    {
+                        _proteins += product.Proteins;
+                    }
+                }
+
+                return _proteins;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException(nameof(value));
+
+                _proteins = value;
+            }
+        }
+        private float _fats;
+        public float Fats
+        {
+            get
+            {
+                if (Products != null)
+                {
+                    foreach (Product product in Products)
+                    {
+                        _fats += product.Fats;
+                    }
+                }
+
+                return _fats;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException(nameof(value));
+
+                _fats = value;
+            }
+        }
+        private float _carbohydrates;
+        public float Carbohydrates
+        {
+            get
+            {
+                if (Products != null)
+                {
+                    foreach (Product product in Products)
+                    {
+                        _carbohydrates += product.Carbohydrates;
+                    }
+                }
+
+                return _carbohydrates;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException(nameof(value));
+
+                _carbohydrates = value;
+            }
+        }
     }
 }
---
+++ CaloriesPFC_Calculator_WPF/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriesPFC_Calculator_WPF.Models
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public float Calories { get; set; }
+        public float Proteins { get; set; }
+        public float Fats { get; set; }
+        public float Carbohydrates { get; set; }
+    }
+}
