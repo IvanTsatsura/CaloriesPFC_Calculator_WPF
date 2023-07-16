8b4c7d347956e2e6b04176f32ba285668cd63de0
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-16 22:15:56 +0200

    enum meal

---
+++ CaloriesPFC_Calculator_WPF/Infrastracture/Enums/Meal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriesPFC_Calculator_WPF.Infrastracture.Enums
+{
+    internal enum Meal
+    {
+        Breakfast,
+        Brunch,
+        Elevenses,
+        Lunch,
+        Linner,
+        Dinner,
+        Supper,
+        Snack
+    }
+}
