4a9aa1e4d03661d9ebbac3d558a93b35689dc7ac
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-17 12:38:51 +0200

    Change Product class Add private fields and checks for values in properties

--- CaloriesPFC_Calculator_WPF/Models/Product.cs
+++ CaloriesPFC_Calculator_WPF/Models/Product.cs
@@ -9,9 +9,82 @@ namespace CaloriesPFC_Calculator_WPF.Models
     internal class Product
     {
         public string Name { get; set; }
-        public float Calories { get; set; }
-        public float Proteins { get; set; }
-        public float Fats { get; set; }
-        public float Carbohydrates { get; set; }
+        private float _calories;
+        public float Calories
+        {
+            get
+            {
+                if (_weightGrams == 100)
+                    return _calories;
+                else
+                    return _calories * _weightGrams / 100;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException(nameof(value));
+                _calories = value;
+            }
+        }
+        private float _proteins;
+        public float Proteins
+        {
+            get
+            {
+                if (_weightGrams == 100)
+                    return _proteins;
+                else
+                    return _proteins * _weightGrams / 100;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException(nameof(value));
+                _proteins = value;
+            }
+        }
+        private float _fats;
+        public float Fats
+        {
+            get
+            {
+                if (_weightGrams == 100)
+                    return _fats;
+                else
+                    return _fats * _weightGrams / 100;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException(nameof(value));
+            }
+        }
+        private float _carbohydrates;
+        public float Carbohydrates 
+        {
+            get
+            {
+                if (_weightGrams == 100)
+                    return _carbohydrates;
+                else
+                    return _carbohydrates * _weightGrams / 100;
+            }
+            set
+            {
+                if (value < 0) 
+                    throw new ArgumentException(nameof(value));
+                _carbohydrates = value;
+            }
+        }
+        private float _weightGrams;
+        public float WeightGrams
+        { 
+            get => _weightGrams;
+            set 
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+                _weightGrams = value;
+            } 
+        }
     }
 }
