1a4935acb7d5d3dcdd9b5ea7f0186438ac3bf186
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-22 15:16:40 +0200

    Create weight validation rule class

---
+++ CaloriesPFC_Calculator_WPF/Infrastracture/ValidationRules/WeightValidationRule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace CaloriesPFC_Calculator_WPF.Infrastracture.ValidationRules
+{
+    class WeightValidationRule : ValidationRule
+    {
+        private const float MIN_POS_WEIGHT = 0.0f;
+
+        public WeightValidationRule()
+        {
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            float weight = MIN_POS_WEIGHT;
+            try
+            {
+                if (((string)value).Length >= 0)
+                    weight = float.Parse((string)value);
+            }
+            catch (Exception e) 
+            {
+                return new ValidationResult(false, $"Illegal characters or {e.Message}");
+            }
+
+            if (weight < MIN_POS_WEIGHT)
+            {
+                return new ValidationResult(false, $"Please enter a weight greater than {MIN_POS_WEIGHT}");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
