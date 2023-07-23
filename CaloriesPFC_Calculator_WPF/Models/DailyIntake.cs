992fcf36665a3e672b7d673f8c5eb827a0a20987
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-24 00:32:42 +0200

    Create DailyIntake class

---
+++ CaloriesPFC_Calculator_WPF/Models/DailyIntake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriesPFC_Calculator_WPF.Models
+{
+    class DailyIntake
+    {
+        public float DailyCaloriesIntake { get; set; }
+        public float DailyProteinsIntake { get; set; }
+        public float DailyFatsIntake { get; set; }
+        public float DailyCarbohydratesIntake { get; set; }
+    }
+}
