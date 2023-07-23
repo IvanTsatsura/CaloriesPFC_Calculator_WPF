e13ac9a224f3e1265f8c1e0ef37bfd4a078cc3ad
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-24 00:35:02 +0200

    Add constructors for DailyIntake class

--- CaloriesPFC_Calculator_WPF/Models/DailyIntake.cs
+++ CaloriesPFC_Calculator_WPF/Models/DailyIntake.cs
@@ -12,5 +12,16 @@ namespace CaloriesPFC_Calculator_WPF.Models
         public float DailyProteinsIntake { get; set; }
         public float DailyFatsIntake { get; set; }
         public float DailyCarbohydratesIntake { get; set; }
+
+        public DailyIntake()
+        {
+        }
+        public DailyIntake(float calories, float proteins, float fats, float carbohydrates)
+        {
+            DailyCaloriesIntake = calories;
+            DailyProteinsIntake = proteins;
+            DailyFatsIntake = fats;
+            DailyCarbohydratesIntake = carbohydrates;
+        }
     }
 }
