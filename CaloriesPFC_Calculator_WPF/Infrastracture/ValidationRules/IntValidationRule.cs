3b58dd800dcd2e87a7b0f7a0d567ebb587d6d496
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-24 22:00:46 +0200

    Create IntValidationRule class

---
+++ CaloriesPFC_Calculator_WPF/Infrastracture/ValidationRules/IntValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace CaloriesPFC_Calculator_WPF.Infrastracture.ValidationRules
+{
+    internal class IntValidationRule : ValidationRule
+    {
+        public IntValidationRule()
+        {
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string pattern = @"^\d+$";
+            Regex regex = new Regex(pattern);
+            try
+            {
+                if (((string)value).Length > 0)
+                    if (!regex.IsMatch((string)value))
+                        throw new Exception();
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, $"Illegal characters or {e.Message}");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
