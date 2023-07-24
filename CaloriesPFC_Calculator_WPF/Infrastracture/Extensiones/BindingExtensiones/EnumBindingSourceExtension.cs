348c20f2d5d04d4d9dc5a46eb5929a83573a40d9
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-24 23:36:36 +0200

    Creeate EnumBindingSourceExtension class

---
+++ CaloriesPFC_Calculator_WPF/Infrastracture/Extensiones/BindingExtensiones/EnumBindingSourceExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace CaloriesPFC_Calculator_WPF.Infrastracture.Extensiones.BindingExtensiones
+{
+    internal class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            if (enumType is null || !enumType.IsEnum)
+                throw new Exception("parameter enumType must not be null and of type Enum");
+            EnumType = enumType;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
