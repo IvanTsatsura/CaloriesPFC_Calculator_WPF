88c5a5c81bcf714ee1b2493467e749cc6e9e9221
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-24 13:16:02 +0200

    Create calculator class and some changes

--- CaloriesPFC_Calculator_WPF/Infrastracture/Enums/Sex.cs
+++ CaloriesPFC_Calculator_WPF/Infrastracture/Enums/Sex.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CaloriesPFC_Calculator_WPF.Infrastracture.Enums
+﻿namespace CaloriesPFC_Calculator_WPF.Infrastracture.Enums
 {
     internal enum Sex
     {
