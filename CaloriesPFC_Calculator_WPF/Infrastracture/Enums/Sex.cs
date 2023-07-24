6983e0894a74090aae9a280e82bb47827be3966c
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-07-24 10:48:02 +0200

    Create sex enum

---
+++ CaloriesPFC_Calculator_WPF/Infrastracture/Enums/Sex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriesPFC_Calculator_WPF.Infrastracture.Enums
+{
+    internal enum Sex
+    {
+        Male,
+        Female
+    }
+}
